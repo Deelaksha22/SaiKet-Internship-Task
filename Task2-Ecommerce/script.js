0c961126ec50144ff907e4e639515a192d534640
Deelaksha NJ <deelaksha22@gmail.com>
2026-03-25 22:36:38 +0530

    Added advanced ecommerce features

---
+++ Task2-Ecommerce/script.js
@@ -0,0 +1,24 @@
+function addToCart() {
+    alert("Product added to cart!");
+}
+let count = 1;
+
+function increase() {
+    count++;
+    document.getElementById("qty").innerText = count;
+}
+
+function decrease() {
+    if(count > 1) {
+        count--;
+        document.getElementById("qty").innerText = count;
+    }
+}
+
+function addToCart() {
+    alert("Added " + count + " item(s) to cart!");
+}
+
+function wishlist() {
+    alert("Added to wishlist ❤️");
+}
